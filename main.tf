d6e7557e617783710ac72a4a39e64da9efb26517
Isabella Barrera <120070445+isabellalejandra@users.noreply.github.com>
2024-01-11 10:58:38 -0800

    change egress protocol

--- main.tf
+++ main.tf
@@ -29,8 +29,8 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   egress {
-    from_port        = 22
-    to_port          = 22
+    from_port        = 0
+    to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = ["::/0"]
